da764f735f1e0182945f701f6b44ca0e00e44066
19lemabe94 <martiinsleonard94@outlook.coom>
2025-06-02 18:34:36 -0300

    Adicionado WPP/ Edição do sobre/ Add meta  name descr.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
+/* CONFIGURAÇÕES DO MENU HAMBURGUER*/
 const menuToggle = document.getElementById('menu-toggle');
 const navMenu = document.getElementById('nav-menu');
 
 menuToggle.addEventListener('click', () => {
     navMenu.classList.toggle('active');
     menuToggle.classList.toggle('active');
+});
+
+/*  Deixar o Header com sombra ao 'scrolar'   */
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('.header');
+    header.classList.toggle('scrolled', window.scrollY > 50);
 });
